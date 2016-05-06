3955364df35ab52cf43a6873d12d2f619f68b473
hung-le <hung-le@users.noreply.github.com>
2016-05-06 08:12:51 -0700

    Moving files to the right location.

---
+++ src/main/java/com/hungle/jdbc/msmoneyaccess/App.java
@@ -0,0 +1,13 @@
+package com.hungle.jdbc.msmoneyaccess;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
